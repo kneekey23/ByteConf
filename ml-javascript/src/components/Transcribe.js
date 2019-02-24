c33c25b8d39d8f1c556e973569b8c28422dbb5b7
Klein <nickik@88e9fe53292e.ant.amazon.com>
2019-02-23 22:36:01 -0800

    saving transcribe progress

--- ml-javascript/src/components/Transcribe.js
+++ ml-javascript/src/components/Transcribe.js
@@ -1,55 +1,106 @@
 import React, {Component} from 'react';
-import { ReactMic } from 'react-mic';
+import RecorderJS from 'recorder-js';
+import { getAudioStream, exportBuffer } from '../utilities/audio';
 var AWS = require('aws-sdk');
 AWS.config.region = 'us-east-1'; 
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: 'us-east-1:1956382a-b3f6-472c-9a8d-3a246853c917'});
 
 
-var transcribeservice = new AWS.TranscribeService({apiVersion: '2017-10-26'});
 
 class Transcribe extends Component {
     constructor(props){
         super(props);
         this.state = {
-            record: false
+            stream: null,
+            recording: false,
+            recorder: null
         }
+        this.startRecord = this.startRecord.bind(this);
+        this.stopRecord = this.stopRecord.bind(this);
        
     }
 
-    startRecording = () => {
+    async componentDidMount() {
+        let stream;
+    
+        try {
+          stream = await getAudioStream();
+        } catch (error) {
+          // Users browser doesn't support audio.
+          // Add your handler here.
+          console.log(error);
+        }
+    
+        this.setState({ stream });
+      }
+   
+      startRecord() {
+        const { stream } = this.state;
+    
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const recorder = new RecorderJS(audioContext);
+        recorder.init(stream);
+    
+        this.setState(
+          {
+            recorder,
+            recording: true
+          },
+          () => {
+            recorder.start();
+          }
+        );
+      }
+     
+      async stopRecord() {
+        const { recorder } = this.state;
+    
+        const { buffer } = await recorder.stop()
+        const audio = exportBuffer(buffer[0]);
+    
+        // Process the audio here.
+        console.log(audio);
+    
         this.setState({
-          record: true
+          recording: false
         });
-      }
-     
-      stopRecording = () => {
-        this.setState({
-          record: false
-        });
-      }
-     
-      onData(recordedBlob) {
-        console.log('chunk of real-time data is: ', recordedBlob);
-      }
-     
-      onStop(recordedBlob) {
-        console.log('recordedBlob is: ', recordedBlob);
+
+        var transcribeservice = new AWS.TranscribeService({apiVersion: '2017-10-26'});
+        var params = {
+            LanguageCode: "en-US", /* required */
+            Media: { /* required */
+              MediaFileUri: 'STRING_VALUE'
+            },
+            MediaFormat: "wav", /* required */
+            TranscriptionJobName: 'BYTECONF_1', /* required */
+            MediaSampleRateHertz: 0
+          };
+          transcribeservice.startTranscriptionJob(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else{
+                console.log(data.size); 
+            }          // successful response
+          });
       }
 
     render() {
+        const { recording, stream } = this.state;
+
+        // Don't show record button if their browser doesn't support it.
+        if (!stream) {
+        return null;
+        }
         return (
         <div className="container">
          <h1>Hello, ByteConf!</h1>
         <div className="row">
-        <ReactMic
-          record={this.state.record}
-          className="sound-wave"
-          onStop={this.onStop}
-          onData={this.onData}
-          strokeColor="#000000"
-          backgroundColor="#FF4081" />
-        <button onClick={this.startRecording} type="button">Start</button>
-        <button onClick={this.stopRecording} type="button">Stop</button>
+        <button
+        onClick={() => {
+          recording ? this.stopRecord() : this.startRecord();
+        }}
+        >
+        {recording ? 'Stop Recording' : 'Start Recording'}
+      </button>
         </div>
         </div>)
     }
