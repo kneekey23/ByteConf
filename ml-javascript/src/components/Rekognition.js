a3e1fb4db17d70c03e3fbc591d4981bb828317a3
Klein <nickik@88e9fe53292e.ant.amazon.com>
2019-03-01 14:02:59 -0800

    forgot to save these changes

--- ml-javascript/src/components/Rekognition.js
+++ ml-javascript/src/components/Rekognition.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
 
 class Rekognition extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        }
+    }
     render(){
         return (
             <div className="container">
--- ml-javascript/src/components/Translate.js
+++ ml-javascript/src/components/Translate.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
 
 class Translate extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            
+        }
+    }
     render() {
         return(
             <div className="container">
