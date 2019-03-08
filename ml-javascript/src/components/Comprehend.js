6a8d868bd9c1b3aaaf7fd99edd3f2574f092797e
Nicholas Walsh <nicholaswalsh.official@gmail.com>
2019-03-08 09:07:17 -0800

    Add sentiment score as output

--- ml-javascript/src/components/Comprehend.js
+++ ml-javascript/src/components/Comprehend.js
@@ -10,7 +10,8 @@ class Comprehend extends Component {
         this.state = {
             text: '',
             resultMessage: '',
-            resultSentiment: ''
+            resultSentiment: '',
+            resultSentimentScore: ''
         }
         this.onChangeText = this.onChangeText.bind(this);
         this.sendTextToComprehend = this.sendTextToComprehend.bind(this);
@@ -36,10 +37,18 @@ class Comprehend extends Component {
         comprehend.detectSentiment(comprehendParams, function (err, data){
             if (err) {
                 currentComponent.setState({resultMessage: err.message});
+                currentComponent.setState({resultSentiment: ""})
+                currentComponent.setState({resultSentimentScore: ""});
             }
             else {
-                currentComponent.setState({resultSentiment: data.Sentiment});
                 currentComponent.setState({resultMessage: "Text analyzed!"})
+                currentComponent.setState({resultSentiment: data.Sentiment});
+                currentComponent.setState({resultSentimentScore: JSON.stringify(data.SentimentScore)});
+                /*
+                for (var key in data.SentimentScore)
+                {
+                this.state.resultSentimentScore.push(data.SentimentScore[key]);
+                }*/
             }
         });
 
@@ -47,9 +56,11 @@ class Comprehend extends Component {
 
 
     render() {
-        let result;
+        let result, response, score;
         if(this.state.resultMessage !== ''){
           result = <p>{this.state.resultMessage}</p>
+          response = <p>{this.state.resultSentiment}</p> 
+          score = <pre>{this.state.resultSentimentScore}</pre>
         }
         return (
           <div className="App">
@@ -59,13 +70,15 @@ class Comprehend extends Component {
                 <div className="col-md-6">
                   <form>
                       <div className="form-group">
-                          <input type="text" className="form-control" value={this.state.text} onChange={this.onChangeText} placeholder="Enter the text for Comprehend to analyze!"/>
+                          <textarea class="form-control" rows="5" value={this.state.text} onChange={this.onChangeText} placeholder="Enter the text for Comprehend to analyze!"/>
                       </div>
                       <button type="button" className="btn btn-success" onClick={this.sendTextToComprehend}>Analyze text with Comprehend</button>
                     </form>
                   </div>
                   <div className="col-md-6">
                 {result}
+                {response}
+                {score}
                 </div>
                 </div>
               </div>
